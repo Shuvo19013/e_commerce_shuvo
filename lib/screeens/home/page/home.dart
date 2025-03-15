67825372af5c8f0789149df8dd5a1b773bad52c5
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-03-15 11:20:28 +0600

    cart_add

--- lib/screeens/home/page/home.dart
+++ lib/screeens/home/page/home.dart
@@ -1,5 +1,5 @@
-import 'package:ecommerce_nahid/common/widgets/appbar.dart';
 import 'package:flutter/material.dart';
+import '../../../common/widget/appbar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,8 +7,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: FAppBar(
-      title: "New Title",
-    ));
+      appBar: FAppBar(title: "New Title", backButton: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/cart.png', // Make sure to add the image in your assets folder
+              width: 200, // Adjust size as needed
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "TMA-2 HD Wireless",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "Rp. 1.500.000",
+              style: TextStyle(fontSize: 16, color: Colors.red),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.star, color: Colors.amber),
+                SizedBox(width: 5),
+                Text("4.6  86 Reviews"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
