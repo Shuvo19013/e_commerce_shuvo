88df51e014f501f49157260e4cc49b348e25f4f5
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-03-15 10:43:54 +0600

    seperated appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-import 'screeens/home/page/home.dart';
+import 'screens/home/page/home.dart';
+
 void main() {
   runApp(const FreebiesEcom());
 }
+
 class FreebiesEcom extends StatelessWidget {
   const FreebiesEcom({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:"Freebies Ecom",
+      debugShowCheckedModeBanner: false,
+      title: "Freebies Ecom",
       home: Home(),
     );
   }
--- lib/screeens/home/page/home.dart
+++ lib/screeens/home/page/home.dart
@@ -1,6 +1,5 @@
+import 'package:ecommerce_nahid/common/widgets/appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
-import '../../../utils/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,27 +7,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Mega Shop",
-          style: TextStyle(
-            fontFamily: "DMSans",
-            fontWeight: FontWeight.bold,
-            color: FColors.oceanBlue,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          Container(
-            padding: EdgeInsets.all(12),
-            child: Icon(Iconsax.notification_bing_outline),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Icon(Icons.shopping_cart),
-          ),
-        ],
-      ),
-    );
+        appBar: FAppBar(
+      title: "New Title",
+    ));
   }
 }
